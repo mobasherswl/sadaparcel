83ae78b57ce85be29d3519a59a710e2f40e6fa4a
Mobasher Khan <mobasher.khan@confiz.com>
2023-04-05 17:42:38 +0500

    offers controller impl

--- src/main/java/com/sadapay/sadaparcel/offers/OffersRestController.java
+++ src/main/java/com/sadapay/sadaparcel/offers/OffersRestController.java
@@ -3,14 +3,18 @@ package com.sadapay.sadaparcel.offers;
 
 import com.sadapay.sadaparcel.api.definition.offers.OffersApi;
 import com.sadapay.sadaparcel.api.model.offers.GetOffers200Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class OffersRestController implements OffersApi {
+
+    private final OffersService offersService;
 
     @Override
     public ResponseEntity<GetOffers200Response> getOffers() {
-        return ResponseEntity.ok(new GetOffers200Response());
+        return ResponseEntity.ok(new GetOffers200Response(offersService.getAll()));
     }
 }
